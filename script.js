bbf649e2b5ac93f76d8f9043a9fb2b7127442f0a
Rafael Angelo <rlangelo@wpi.edu>
2015-12-07 18:58:22 -0500

    Adds all events and button functionality

--- script.js
+++ script.js
@@ -1,139 +1,79 @@
-"use strict";
-function yearFunction() {
-    document.getElementById("subButton").disabled = true;
-    var currentYear = new Date().getFullYear();
-    var sel = document.getElementById('year');
+document.getElementById("studButton").addEventListener("click", displayStudent);
+document.getElementById("staffBut").addEventListener("click", displayStaff);
+var studOrStaff = -1;
+document.getElementById("Cancel").addEventListener("click", cancelButton);
+document.getElementById("inner").addEventListener("click", Hobby, false);
+document.getElementById("middle").addEventListener("click", Age, false);
+document.getElementById("outer").addEventListener("click", Department, false);
+document.getElementById("inner2").addEventListener("click", sHobby, true);
+document.getElementById("middle2").addEventListener("click", sAge, true);
+document.getElementById("outer2").addEventListener("click", sDepartment, true);
+document.getElementById("staffBut").addEventListener("mouseover", colorChange1);
+document.getElementById("studButton").addEventListener("mouseover", colorChange2);
+document.getElementById("staffBut").addEventListener("mouseout", colorRestore1);
+document.getElementById("studButton").addEventListener("mouseout", colorRestore2);
 
-    var option1 = document.createElement('option');
-    option1.appendChild(document.createTextNode(currentYear));
-    option1.value = 'thisYear';
-    sel.appendChild(option1);
 
-    var seniors = currentYear + 1;
-    var option2 = document.createElement('option');
-    option2.appendChild(document.createTextNode(seniors));
-    option2.value = 'seniorYear';
-    sel.appendChild(option2);
-
-    var juniors = seniors + 1;
-    var option3 = document.createElement('option');
-    option3.appendChild(document.createTextNode(juniors));
-    option3.value = 'juniorYear';
-    sel.appendChild(option3);
-
-    var sophomores = juniors + 1;
-    var option4 = document.createElement('option');
-    option4.appendChild(document.createTextNode(sophomores));
-    option4.value = 'sophomoreYear';
-    sel.appendChild(option4);
-
-    var freshmen = sophomores + 1;
-    var option5 = document.createElement('option');
-    option5.appendChild(document.createTextNode(freshmen));
-    option5.value = 'freshmenYear';
-    sel.appendChild(option5);
+function colorRestore1() {
+	document.getElementById("staffBut").style.background= '';
 }
 
-function submitFunction() {
-    var e = document.getElementById("year");
-    if(!document.getElementById("title").value || !document.getElementById("description").value)
-    {
-        document.getElementById("error").style.visibility = "visible";
-        document.getElementById("nofile").style.visibility = "hidden";
-    }
-    else if(!document.getElementById("asset").value) {
-        document.getElementById("nofile").style.visibility = "visible";
-        document.getElementById("error").style.visibility = "hidden";
-    }
-    else {
-        document.getElementById("error").style.visibility = "hidden";
-        document.getElementById("nofile").style.visibility = "hidden";
-        parseTable(document.getElementById("author_table"));
-        var submitButton = document.getElementById("submit_button");
-        submit_button.click();
-    }
+function colorRestore2() {
+	document.getElementById("studButton").style.background = '';
 }
 
-function addAuthor() {
-    var selection = document.getElementById("year");
-    if (!document.getElementById("author").value ||
-        !document.getElementById("username").value ||
-         selection.options[selection.selectedIndex].value == "blank") {
-        document.getElementById("error").style.visibility = "hidden";
-        document.getElementById("nofile").style.visibility = "hidden";
-        document.getElementById('autError').style.visibility = "visible";
-    }
-    else {
-    document.getElementById("error").style.visibility = "hidden";
-    document.getElementById("nofile").style.visibility = "hidden";
-    document.getElementById('autError').style.visibility = "hidden";
-    var table = document.getElementById("author_table");
-    var row = table.insertRow(0);
-
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-
-    cell1.innerHTML = document.getElementById("author").value;
-    cell2.innerHTML = document.getElementById("username").value;
-    cell3.innerHTML = selection.options[selection.selectedIndex].text;
-    document.getElementById("author").value = "";
-    document.getElementById("username").value = "";
-    selection.selectedIndex = "0";
-    document.getElementById("subButton").disabled = false;
-    }
+function colorChange1() {
+	document.getElementById("staffBut").style.background='lightgreen';
 }
 
-function parseTable(theTable) {
-    var tLength = theTable.rows.length;
-    var selection = document.getElementById("year");
-    if (tLength == 1) {
-        document.getElementById("author").value = theTable.rows[0].cells[0].innerText;
-        document.getElementById("username").value = theTable.rows[0].cells[1].innerText;
-        var userYear = theTable.rows[0].cells[2].innerText;
-        var field = document.createElement("input");
-        field.setAttribute("type", "hidden");
-        field.setAttribute("id", "gYear");
-        field.setAttribute("name", "Graduation Year");
-        field.setAttribute("value", userYear);
-        selection.selectedIndex = "0";
-        document.getElementById("form1").appendChild(field);
-        document.getElementById("form1").removeChild(selection);
-    }
-    else {
-        var name = "";
-        var username = "";
-        var year = "";
-        var nameField;
-        var userNameField;
-        var yearField;
-        for (var i=0;i<tLength;i++) {
-            name = theTable.rows[i].cells[0].innerText;
-            username = theTable.rows[i].cells[1].innerText;
-            year = theTable.rows[i].cells[2].innerText;
+function colorChange2() {
+	document.getElementById("studButton").style.background = 'lightgreen';
+}
 
-            nameField = document.createElement("input");
-            nameField.setAttribute("type", "hidden");
-            nameField.setAttribute("name", "Author " + (i+1));
-            nameField.setAttribute("value", name);
-            document.getElementById("form1").appendChild(nameField);
+function displayStudent() {
+	document.getElementById("outer").style.visibility = "visible";
+	document.getElementById("staffBut").disabled = true;
+	studOrStaff = 0;
+}
 
-            userNameField = document.createElement("input");
-            userNameField.setAttribute("type", "hidden");
-            userNameField.setAttribute("name", "Username " + (i+1));
-            userNameField.setAttribute("value", username);
-            document.getElementById("form1").appendChild(userNameField);
+function displayStaff() {
+	document.getElementById("outer2").style.visibility = "visible";
+	document.getElementById("studButton").disabled = true;
+	studOrStaff = 1;
+}
 
-            yearField = document.createElement("input");
-            yearField.setAttribute("type", "hidden");
-            yearField.setAttribute("name", "Graduation Year " + (i+1));
-            yearField.setAttribute("value", year);
-            document.getElementById("form1").appendChild(yearField);
+function cancelButton() {
+	document.getElementById("outer").style.visibility = "hidden";
+	document.getElementById("outer2").style.visibility = "hidden";
+	if (studOrStaff == 0) {
+		document.getElementById("staffBut").disabled = false;
+	}
+	else {
+		document.getElementById("studButton").disabled = false;
+	}
+	studOrStaff = -1;
+}
 
-        }
+function Hobby() {
+	alert("inner");
+}
 
-            document.getElementById("form1").removeChild(document.getElementById("year"));
-        document.getElementById("form1").removeChild(document.getElementById("author"));
-        document.getElementById("form1").removeChild(document.getElementById("username"));
-    }
+function Age() {
+	alert("middle");
+}
+
+function Department() {
+	alert("outer");
+}
+
+function sHobby() {
+	alert("inner");
+}
+
+function sAge() {
+	alert("middle");
+}
+
+function sDepartment() {
+	alert("outer");
 }
