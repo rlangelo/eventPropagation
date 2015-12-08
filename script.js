94b00bc4d3029d7f5ccbea5fc729ea7f937f6724
Rafael Angelo <rlangelo@wpi.edu>
2015-12-07 22:28:59 -0500

    Works perfectly. Nedds layouting

--- script.js
+++ script.js
@@ -5,14 +5,22 @@ document.getElementById("Cancel").addEventListener("click", cancelButton);
 document.getElementById("inner").addEventListener("click", Hobby, false);
 document.getElementById("middle").addEventListener("click", Age, false);
 document.getElementById("outer").addEventListener("click", Department, false);
-document.getElementById("inner2").addEventListener("click", sHobby, true);
+document.getElementById("inner2").addEventListener("click", Hobby, true);
 document.getElementById("middle2").addEventListener("click", sAge, true);
-document.getElementById("outer2").addEventListener("click", sDepartment, true);
+document.getElementById("outer2").addEventListener("click", Department, true);
 document.getElementById("staffBut").addEventListener("mouseover", colorChange1);
 document.getElementById("studButton").addEventListener("mouseover", colorChange2);
 document.getElementById("staffBut").addEventListener("mouseout", colorRestore1);
 document.getElementById("studButton").addEventListener("mouseout", colorRestore2);
+document.getElementById("subButton").addEventListener("click", submitName);
 
+
+function submitName() {
+	var value = document.getElementById("charName").value;
+	document.getElementById("name").innerText = value;
+	document.getElementById("subButton").disabled = true;
+	document.getElementById("cName").style.visibility = "visible";
+}
 
 function colorRestore1() {
 	document.getElementById("staffBut").style.background= '';
@@ -34,17 +42,26 @@ function displayStudent() {
 	document.getElementById("outer").style.visibility = "visible";
 	document.getElementById("staffBut").disabled = true;
 	studOrStaff = 0;
+	document.getElementById("type").innerText = "Student";
 }
 
 function displayStaff() {
 	document.getElementById("outer2").style.visibility = "visible";
 	document.getElementById("studButton").disabled = true;
 	studOrStaff = 1;
+	document.getElementById("type").innerText = "Staff";
 }
 
 function cancelButton() {
 	document.getElementById("outer").style.visibility = "hidden";
 	document.getElementById("outer2").style.visibility = "hidden";
+	document.getElementById("subButton").disabled = false;
+	document.getElementById("name").innerText = "";
+	document.getElementById("type").innerText = "";
+	document.getElementById("cName").style.visibility = "hidden";
+	document.getElementById("dept").style.visibility = "hidden";
+	document.getElementById("age").style.visibility = "hidden";
+	document.getElementById("hobby").style.visibility = "hidden";
 	if (studOrStaff == 0) {
 		document.getElementById("staffBut").disabled = false;
 	}
@@ -54,26 +71,65 @@ function cancelButton() {
 	studOrStaff = -1;
 }
 
+var departments = ["Actuarial Mathematics", "Aerospace Engineering", "Architectural Engineering", "Biochemistry", "Biomedical Engineering", "Chemical Engineering", "Civil Engineering", "Computer Science", "Electrical and Computer Engineering", "Environmental Engineering", "Fire Protection Engineering", "Industrial Engineering", "Manufacturing Engineering", "Mechanical Engineering", "Robotics Engineering"];
+
+var hobbies = ["3D printing", "Amateur Radio", "Acting", "Board Games", "Book Restoration", "Cabaret", "Calligraphy", "Candle Making", "Coffee Roasting", "Cooking", "Coloring", "Cosplaying", "Couponing", "Creative Writing", "Crocheting", "Cryptography", "Dancing", "Digital Arts", "Drama", "Drawing", "Do it Yourself", "Embroidery", "Flower Arrangement", "Foreign Language Learning", "RPGs", "Gambling", "Genealogy", "Glassblowing", "Gunsmithing", "Homebrewing", "Ice Skating", "Jewerly Making", "Jigsaw Puzzles", "Juggling", "Knapping", "Knitting", "Knifemaking", "Lacemaking", "Lapidary", "Leather Crafting", "Lego Building", "Lockpicking", "Machining", "Metalworking", "Magic", "Model Building", "Listening to Music", "Origami", "Painting", "Playing Instruments", "Pet", "Pottery", "Quilting", "Reading", "Scrapbooking", "Sculpting", "Sewing", "Singing", "Sketching", "Soapmaking", "Stand-up Comedy", "Sudoku", "Table Tennis", "Taxidermy", "Video Games", "Watching Movies", "Web Surfing", "Whittling", "Wood Carving", "Woodworking", "Writing", "Yoga", "Yo-yoing"];
+
 function Hobby() {
-	alert("inner");
+	if (studOrStaff == 0) {
+		setTimeout(function(){
+			var value = getRandomInt(0, hobbies.length-1);
+			var hobby = hobbies[value];
+			document.getElementById("randHobby").innerText = hobby;
+			document.getElementById("hobby").style.visibility = "visible";
+		}, 1000);
+	} else {
+		setTimeout(function(){
+			var value = getRandomInt(0, hobbies.length-1);
+			var hobby = hobbies[value];
+			document.getElementById("randHobby").innerText = hobby;
+			document.getElementById("hobby").style.visibility = "visible";
+		}, 3000);
+	}
 }
 
 function Age() {
-	alert("middle");
+	setTimeout(function(){
+	var age = getRandomInt(17, 28);
+	document.getElementById("randAge").innerText = String(age);
+	document.getElementById("age").style.visibility = "visible";
+	}, 2000);
 }
 
 function Department() {
-	alert("outer");
-}
-
-function sHobby() {
-	alert("inner");
+	if (studOrStaff == 0){
+		setTimeout(function(){
+			var value = getRandomInt(0, departments.length-1);
+			var department = departments[value];
+			document.getElementById("randDept").innerText = department;
+			document.getElementById("dept").style.visibility = "visible";
+		}, 3000);
+	}
+	else {
+		setTimeout(function(){
+			var value = getRandomInt(0, departments.length-1);
+			var department = departments[value];
+			document.getElementById("randDept").innerText = department;
+			document.getElementById("dept").style.visibility = "visible";
+		}, 1000);
+	}
 }
 
 function sAge() {
-	alert("middle");
+	setTimeout(function(){
+		var age = getRandomInt(27, 80);
+		document.getElementById("randAge").innerText = String(age);
+		document.getElementById("age").style.visibility = "visible";
+	}, 2000);
+	
 }
 
-function sDepartment() {
-	alert("outer");
+function getRandomInt(min, max) { 
+	var number = Math.floor(Math.random() * (max - min + 1)) + min;
+	return number;
 }
